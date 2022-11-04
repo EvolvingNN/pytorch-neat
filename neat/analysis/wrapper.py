3d8b14285f8dbc1ba951594d8f7d7e9fa8d2b778
Gregory Conrad <gregorysconrad@gmail.com>
2022-11-04 13:21:16 -0400

    Refactored the analysis code slightly

--- neat/analysis/wrapper.py
+++ neat/analysis/wrapper.py
@@ -1,22 +1,11 @@
 import pandas as pd
 
-from neat.analysis.algorithms import (
-    random_selection_accuracies,
-    greedy_1_selection_accuracies,
-    greedy_2_selection_accuracies,
-    diversity_rr_selection_accuracies,
-)
+from neat.analysis.algorithms import ALGORITHMS
 
 
 def run_trial_analysis(final_population_prediction_map, ensemble_evaluator):
-    algorithms_to_run = {
-        "random": random_selection_accuracies,
-        "greedy1": greedy_1_selection_accuracies,
-        "greedy2": greedy_2_selection_accuracies,
-        "diversity": diversity_rr_selection_accuracies,
-    }
     algorithm_results = {
         name: algo(final_population_prediction_map, ensemble_evaluator)
-        for name, algo in algorithms_to_run.items()
+        for name, algo in ALGORITHMS.items()
     }
     return pd.DataFrame(algorithm_results)
