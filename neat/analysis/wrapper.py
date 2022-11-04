148c2830646b3eba344232bba0a89fafba008a79
Gregory Conrad <gregorysconrad@gmail.com>
2022-11-04 12:35:30 -0400

    Added analysis wrapper runner

---
+++ neat/analysis/wrapper.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from analysis.algorithms import *
+
+
+def run_analysis(final_population_prediction_map, ensemble_evaluator):
+    algorithms_to_run = {
+        "random": random_selection_accuracies,
+        "greedy1": greedy_1_selection_accuracies,
+        "greedy2": greedy_2_selection_accuracies,
+        "diversity": diversity_rr_selection_accuracies,
+    }
+    algorithm_results = {
+        name: algo(final_population_prediction_map, ensemble_evaluator)
+        for name, algo in algorithms_to_run.items()
+    }
+    return pd.DataFrame(algorithm_results)
