dd98e6919b1557cec9d82f921a439bf6d448a5d1
aidanhor-n <107128627+aidanhor-n@users.noreply.github.com>
2023-02-13 06:29:44 -0500

    Update acrobot_run.py

--- acrobot_run.py
+++ acrobot_run.py
@@ -35,8 +35,70 @@ sweep_configuration = {
      }
 }
 
-sweep_id = wandb.sweep(sweep=sweep_configuration, project="acrobot", entity="evolvingnn")
-print(sweep_id)
+# sweep_id = wandb.sweep(sweep=sweep_configuration, project="acrobot", entity="evolvingnn")
+# print(sweep_id)
+
+def control():
+    sweep_configuration = {
+        'method': 'bayes',
+        'name': 'Acrobot Control',
+        'metric': {
+            'goal': 'maximize', 
+            'name': 'Best Fitness'
+            },
+        'parameters': {
+            'USE_BIAS': {'values': [False, True]},
+            'POPULATION_SIZE': {'max': 20, 'min': 3},
+            'CANDIDATE_LIMIT': {'values': [2, 3, 5, 7, 13, 25]},
+            'SCALE_ACTIVATION': {'max': 7, 'min': 2},
+            'SPECIATION_THRESHOLD': {'max' : 5, 'min' : 1},
+            'CONNECTION_MUTATION_RATE': {'max': 1.0, 'min': 0.1},
+            'CONNECTION_PERTURBATION_RATE': {'max': 1.0, 'min': 0.1},
+            'ADD_NODE_MUTATION_RATE': {'max': 1.0, 'min': 0.1},
+            'ADD_CONNECTION_MUTATION_RATE': {'max': 1.0, 'min': 0.1},
+            'CROSSOVER_REENABLE_CONNECTION_GENE_RATE': {'max': 1.0, 'min': 0.1},
+            'PERCENTAGE_TO_SAVE': {'max': 1.0, 'min': 0.1}
+        }
+    }
+
+    #sweep_id = wandb.sweep(sweep=sweep_configuration, project="acrobot", entity="evolvingnn")
+
+    KWARGS['USE_CONTROL'] = True
+    KWARGS['USE_ACER'] = False
+    KWARGS['USE_ACER_WITH_WARMUP'] = False
+    
+    wandb.init(config = KWARGS, group = 'Acrobot Control')
+
+    kwargs = {
+        'VERBOSE': wandb.config.VERBOSE,
+        'NUM_INPUTS': wandb.config.NUM_INPUTS,
+        'NUM_OUTPUTS': wandb.config.NUM_OUTPUTS,
+        'USE_BIAS': wandb.config.USE_BIAS,
+        'GENERATIONAL_ENSEMBLE_SIZE': wandb.config.GENERATIONAL_ENSEMBLE_SIZE,
+        'CANDIDATE_LIMIT': wandb.config.CANDIDATE_LIMIT,
+        'ACTIVATION': wandb.config.ACTIVATION,
+        'SCALE_ACTIVATION': wandb.config.SCALE_ACTIVATION,
+        'MAX_EPISODE_STEPS': wandb.config.MAX_EPISODE_STEPS,
+        'FITNESS_THRESHOLD': wandb.config.FITNESS_THRESHOLD,
+        'TOP_HEIGHT' : wandb.config.TOP_HEIGHT,
+        'USE_CONTROL' : wandb.config.USE_CONTROL,
+        'USE_ACER' : wandb.config.USE_ACER,
+        'USE_ACER_WITH_WARMUP' : wandb.config.USE_ACER_WITH_WARMUP,
+        'POPULATION_SIZE': wandb.config.POPULATION_SIZE,
+        'NUMBER_OF_GENERATIONS': wandb.config.NUMBER_OF_GENERATIONS,
+        'SPECIATION_THRESHOLD': wandb.config.SPECIATION_THRESHOLD,
+        'CONNECTION_MUTATION_RATE': wandb.config.CONNECTION_MUTATION_RATE,
+        'CONNECTION_PERTURBATION_RATE': wandb.config.CONNECTION_PERTURBATION_RATE,
+        'ADD_NODE_MUTATION_RATE': wandb.config.ADD_NODE_MUTATION_RATE,
+        'ADD_CONNECTION_MUTATION_RATE': wandb.config.ADD_CONNECTION_MUTATION_RATE,
+        'CROSSOVER_REENABLE_CONNECTION_GENE_RATE': wandb.config.CROSSOVER_REENABLE_CONNECTION_GENE_RATE,
+        'PERCENTAGE_TO_SAVE': wandb.config.PERCENTAGE_TO_SAVE,
+    }
+    kwargs['wandb'] = wandb
+
+    neat = pop.Population(c.AcrobotBalanceConfig(**kwargs))
+    for solution, generation in neat.run():
+        continue
 
 def test():
     KWARGS['POPULATION_SIZE'] = 5
@@ -122,5 +184,5 @@ if __name__ == '__main__':
     # for _ in range(10):
         # train()
     #train()
-    wandb.agent('ess9zgoz', function=train)
+    wandb.agent('ch3atpcv', function=control)
     #test()
